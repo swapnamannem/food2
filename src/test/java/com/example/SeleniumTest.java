3cada072ed3d41ff3f648ef1c8ff87ddfb79baf8
Sai Shirisha Chevuri <shirishachevuri@Shirishas-MacBook-Air.local>
2024-05-22 17:12:21 +0530

    selenium testing7

--- src/test/java/com/example/SeleniumTest.java
+++ src/test/java/com/example/SeleniumTest.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -20,7 +21,11 @@ public class SeleniumTest {
     @Before
     public void setUp() {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless"); // Ensure Chrome runs in headless mode
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        driver = new ChromeDriver(options);
         wait = new WebDriverWait(driver, 10); // 10 seconds timeout
     }
 
